3611e3e7bd8e018db73604965c3155cf12472527
Safwan Samsudeen <safwansamsudeen.c@gmail.com>
2025-10-07 20:56:35 +0530

    feat: deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "https://safwansamsudeen.github.io",
+  base: "/humans-too",
   integrations: [mdx(), sitemap(), tailwind()],
 });
